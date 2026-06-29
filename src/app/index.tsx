056accefdf35ee189542251ff8c42f54b3623a53
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2026-06-29 21:08:48 +0530

    setup fonts and added a lot of libraries required for the project

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="font-sans-bold">will develop it slowly</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
