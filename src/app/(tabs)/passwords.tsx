a30eb02cceb3f142621d136ebc240312695a6c18
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2026-09-13 22:11:41 +0530

    displaying the list of crates

--- src/app/(tabs)/passwords.tsx
+++ src/app/(tabs)/passwords.tsx
@@ -1,16 +1,40 @@
+import { Button } from "@/components/ui/button";
+import image from "@/constants/images";
 import { useDb } from "@/db/hooks/useDb";
 import { cratesQuery } from "@/db/queries/crates.queries";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { FlatList, Image, Text } from "react-native";
 
 const Passwords = () => {
   const db = useDb();
+  const router = useRouter();
   const { data: crates } = useLiveQuery(cratesQuery(db));
-  console.log("crates", crates);
+
   return (
-    <View>
-      <Text>Passwords</Text>
-    </View>
+    <FlatList
+      data={crates}
+      keyExtractor={(crate) => crate.id}
+      numColumns={2}
+      className="main"
+      columnWrapperStyle={{ gap: 16 }}
+      contentContainerClassName="gap-y-4 pb-4"
+      renderItem={({ item: crate }) => (
+        <Button
+          variant="ghost"
+          onPress={() => router.push(`/crate/${crate.id}`)}
+          className="h-auto flex-1 flex-col items-center gap-y-2"
+        >
+          <Image source={image.crate} className="size-32" />
+          <Text
+            className="text-center text-base font-sans-semibold text-text-primary"
+            numberOfLines={1}
+          >
+            {crate.name}
+          </Text>
+        </Button>
+      )}
+    />
   );
 };
 
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,5 +1,6 @@
+import crate from "@assets/images/crate.png";
 import fingerPrint from "@assets/images/fingerprint.png";
 import key from "@assets/images/key.png";
 import logo from "@assets/images/logo.png";
 
-export default { key, fingerPrint, logo };
+export default { key, fingerPrint, logo, crate };
