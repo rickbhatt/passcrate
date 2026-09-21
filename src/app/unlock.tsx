c1ee000f7fd6b2b252f8f3b0b70a7ff8c609d7fa
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2026-09-22 00:12:31 +0530

    unlock and lock buttons fixed

--- src/app/unlock.tsx
+++ src/app/unlock.tsx
@@ -155,12 +155,11 @@ const UnlockScreen = () => {
         <View className="flex-col gap-y-4 items-center w-full mt-5">
           <Text className="h2-bold text-text-primary">PassCrate Locked</Text>
           <Button
-            variant="outline"
             className="w-full"
             disabled={isBusy}
             onPress={() => biometricAuthRef.current()}
           >
-            <Text className="font-sans-semibold text-base text-text-primary">Unlock</Text>
+            <Text className="btn-label-white">Unlock</Text>
           </Button>
         </View>
       )}
